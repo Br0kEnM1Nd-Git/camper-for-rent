b7357171b594aea19ca8ad79c809a58588da8643
Maxim <mrgamer594@gmail.com>
2024-03-25 01:10:12 +0200

    create redux store

---
+++ src/redux/store.js
@@ -0,0 +1,23 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import persistStore from 'redux-persist/es/persistStore';
+
+const store = configureStore({
+  reducer: () => {},
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export default store;
+export const persistor = persistStore(store);
