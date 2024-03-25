4afb66b0a378a8989500686272fda726597a6f91
Maxim <mrgamer594@gmail.com>
2024-03-26 00:11:55 +0200

    add base styles for button

--- src/styles/components.styled.js
+++ src/styles/components.styled.js
@@ -1,3 +1,13 @@
 import styled from 'styled-components';
 
-// TODO: Зробити базові стилізовані компоненти для найчастіше вживаних елементі, наприклад: input, button, etc...
+export const PrimaryButton = styled.button`
+  border: 0;
+  border-radius: 200px;
+  background-color: var(--button);
+
+  color: #ffffff;
+
+  &:hover {
+    background-color: #d84343;
+  }
+`;
