b492805c1e790a1d4870586a1089b61848903b53
Maxim <mrgamer594@gmail.com>
2024-03-26 08:51:53 +0200

    delete redux usage

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'App';
+import App from 'App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { Provider } from 'react-redux';
-import store, { persistor } from 'redux/store';
-import { PersistGate } from 'redux-persist/lib/integration/react';
+// import { Provider } from 'react-redux';
+// import store, { persistor } from './redux/store';
+// import { PersistGate } from 'redux-persist/lib/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="camper-for-rent">
-      <PersistGate loading={null} persistor={persistor}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </PersistGate>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      {/* <Provider store={store}> */}
+      <App />
+      {/* </Provider> */}
+      {/* </PersistGate> */}
     </BrowserRouter>
   </React.StrictMode>
 );
