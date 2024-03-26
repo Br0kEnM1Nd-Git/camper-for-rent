c3e7a3dae5d8e65cb4bc0e73eb18f3fe4c1e5e86
Maxim <mrgamer594@gmail.com>
2024-03-26 08:51:15 +0200

    add container

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,12 +1,13 @@
 import Header from 'components/Header';
 import { Outlet } from 'react-router-dom';
+import { LayoutContainer } from './Layout.styled';
 
 const Layout = () => {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </div>
+    </LayoutContainer>
   );
 };
 
