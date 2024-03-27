19393c0f5bf6fae16b73ca0cd7d0f33a30e69e2f
Maxim <mrgamer594@gmail.com>
2024-03-27 16:05:54 +0200

    add store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'App';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-// import { Provider } from 'react-redux';
-// import store, { persistor } from './redux/store';
+import { Provider } from 'react-redux';
 // import { PersistGate } from 'redux-persist/lib/integration/react';
+import './index.css';
+import App from 'App';
+import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="camper-for-rent">
       {/* <PersistGate loading={null} persistor={persistor}> */}
-      {/* <Provider store={store}> */}
-      <App />
-      {/* </Provider> */}
+      <Provider store={store}>
+        <App />
+      </Provider>
       {/* </PersistGate> */}
     </BrowserRouter>
   </React.StrictMode>
