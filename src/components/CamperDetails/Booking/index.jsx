09c669c4304867443f4d35a4d317869c9a7f0132
Maxim <mrgamer594@gmail.com>
2024-03-27 16:01:22 +0200

    mvp booking

---
+++ src/components/CamperDetails/Booking/index.jsx
@@ -0,0 +1,51 @@
+import { Field, Form, Formik } from 'formik';
+import {
+  BookingContainer,
+  FormHeader,
+  FormSecondaryText,
+  InputsGroup,
+  SendButton,
+} from './Booking.styled';
+
+const Booking = () => {
+  const initialValues = {
+    name: '',
+    email: '',
+    date: '',
+    comment: '',
+  };
+
+  const handleSubmit = values => {
+    console.log(values);
+
+    window.location.reload();
+  };
+
+  return (
+    <BookingContainer>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form autoComplete="off" noValidate>
+          <FormHeader>Book your campervan now</FormHeader>
+
+          <FormSecondaryText>
+            Stay connected! We are always ready to help you.
+          </FormSecondaryText>
+
+          <InputsGroup>
+            <Field type="text" name="name" id="name" placeholder="Name" />
+
+            <Field type="email" name="email" id="email" placeholder="Email" />
+
+            <Field type="date" name="date" id="date" />
+
+            <Field as="textarea" name="comment" id="comment" rows="5" />
+          </InputsGroup>
+
+          <SendButton type="submit">Send</SendButton>
+        </Form>
+      </Formik>
+    </BookingContainer>
+  );
+};
+
+export default Booking;
