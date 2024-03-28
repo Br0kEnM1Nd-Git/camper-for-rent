9e4993f044d95528a8741870ee112636cc7e5f1b
Maxim <mrgamer594@gmail.com>
2024-03-28 06:50:13 +0200

    delete persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  FLUSH,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-  REHYDRATE,
-} from 'redux-persist';
-import persistStore from 'redux-persist/es/persistStore';
 import rootReducer from './reducer';
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
 
 export default store;
-export const persistor = persistStore(store);
