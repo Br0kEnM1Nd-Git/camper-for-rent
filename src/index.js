c5e7333ec54c25d612a8ab6c0d0d5a88ec69adad
Maxim <mrgamer594@gmail.com>
2024-03-28 06:49:16 +0200

    delete peristsgate

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/lib/integration/react';
 import './index.css';
 import App from 'App';
 import store from './redux/store';
@@ -10,11 +9,9 @@ import store from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="camper-for-rent">
-      {/* <PersistGate loading={null} persistor={persistor}> */}
       <Provider store={store}>
         <App />
       </Provider>
-      {/* </PersistGate> */}
     </BrowserRouter>
   </React.StrictMode>
 );
