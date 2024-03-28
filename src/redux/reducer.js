86eb348842861635b973b8c6ddc9b22c0c2d6ca8
Maxim <mrgamer594@gmail.com>
2024-03-28 08:41:06 +0200

    fix imports after rename

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,12 +1,12 @@
 import advertsReducer from './adverts/advertsSlice';
 import apiReducer from './rootReducers/apiSlice';
-import filterReducer from './filter/filterSlice';
+import filtersReducer from './filters/filterSlice';
 import favoritesReducer from './favorites/favoritesSlice';
 
 const rootReducer = {
   adverts: advertsReducer,
   api: apiReducer,
-  filter: filterReducer,
+  filters: filtersReducer,
   favorites: favoritesReducer,
 };
 
