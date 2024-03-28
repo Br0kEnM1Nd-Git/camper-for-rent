da2a845a5d7be275b2a01d66bf88cf1041773209
Maxim <mrgamer594@gmail.com>
2024-03-28 08:38:56 +0200

    add filteredadverts selector

---
+++ src/redux/adverts/selectors.js
@@ -0,0 +1,42 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { selectFilters } from '../filters/selectors';
+
+const selectAdverts = state => state.adverts;
+
+const selectFilteredAdverts = createSelector(
+  [selectAdverts, selectFilters],
+  (adverts, filters) =>
+    adverts.filter(
+      ({
+        transmission,
+        form,
+        location,
+        details: { kitchen, shower, TV: tv, airConditioner: ac },
+      }) => {
+        console.log(filters);
+        if (!!filters.location) return filters.location === location;
+
+        if (filters.equipment.length > 0) {
+          const equipmentFilters = {};
+          console.log('equipment', equipmentFilters);
+
+          filters.equipment.forEach(el => {
+            equipmentFilters[el] = el;
+          });
+
+          if (!!equipmentFilters.ac) return equipmentFilters.ac === ac;
+          if (!!equipmentFilters.automatic)
+            return equipmentFilters.automatic === transmission;
+          if (!!equipmentFilters.kitchen) return !!kitchen;
+          if (!!equipmentFilters.tv) return !!tv;
+          if (!!equipmentFilters.shower) return !!shower;
+        }
+
+        if (!!filters.type) return filters.type === form;
+
+        return true;
+      }
+    )
+);
+
+export { selectAdverts, selectFilteredAdverts };
