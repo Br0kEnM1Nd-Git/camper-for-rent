400d3b2b1d469a550998c182f182aa405ccd1db6
Maxim <mrgamer594@gmail.com>
2024-03-28 06:49:56 +0200

    delete persist

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,17 +1,13 @@
-// import persistReducer from 'redux-persist/es/persistReducer';
-// import storage from 'redux-persist/lib/storage';
 import advertsReducer from './adverts/advertsSlice';
 import apiReducer from './rootReducers/apiSlice';
-
-// const persistConfig = {
-//   key: 'auth',
-//   storage,
-//   blacklist: [],
-// };
+import filterReducer from './filter/filterSlice';
+import favoritesReducer from './favorites/favoritesSlice';
 
 const rootReducer = {
   adverts: advertsReducer,
   api: apiReducer,
+  filter: filterReducer,
+  favorites: favoritesReducer,
 };
 
 export default rootReducer;
