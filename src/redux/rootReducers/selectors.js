94f9d8ff0a6ee13e60f275b5923759f5aabd7df5
Maxim <mrgamer594@gmail.com>
2024-03-29 07:55:33 +0200

    add selector for full api state

--- src/redux/rootReducers/selectors.js
+++ src/redux/rootReducers/selectors.js
@@ -2,4 +2,6 @@ const selectIsLoading = state => state.api.isLoading;
 
 const selectError = state => state.api.error;
 
-export { selectIsLoading, selectError };
+const selectApiState = state => state.api;
+
+export { selectIsLoading, selectError, selectApiState };
